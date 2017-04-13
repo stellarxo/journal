2af862f2704dcb4634fd49c32ee68084695a56bf
Stella Lee <slee.mcin@gmail.com>
2017-04-12 22:52:11 -0400

    initial users and such

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+
+  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406032910) do
+ActiveRecord::Schema.define(version: 20170413022002) do
 
   create_table "entries", force: :cascade do |t|
     t.text     "summary"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20170406032910) do
     t.integer  "energy_level"
     t.float    "sleep"
     t.float    "meditation"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
